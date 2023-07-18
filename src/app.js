80a589e99b4201379c65b5bc1783b78e2ed78da8
luisparedes29 <luisparedescrv@gmail.com>
2023-07-17 22:26:24 -0400

    Bases del servidor y funcionalidades avanzadas
    - Creada estructura de carpetas
    - Agregada dependencia util para la seguridad del servidor como lo es helmet
    - Agregada dependencia para comprimir el envio de la cabecera(compression)
    - Agregado Clustering para mejor rendimiento, mayor capacidad de carga, Resistencia a fallos y escabilidad vertical

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const compression = require('compression')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(compression())
+app.use(helmet())
+app.disable('x-powered-by')
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+app.use((req, res, next) => {
+  res.status(404).json('No se ha encontrado')
+})
+
+module.exports = app
