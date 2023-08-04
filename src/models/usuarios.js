552376342ed2082f12663311ee6da041d48339fc
luisparedes29 <luisparedescrv@gmail.com>
2023-08-04 00:22:00 -0400

    añadida fecha limite de cancelacion de reservaciones

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,30 +1,35 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const usuariosSchema = new Schema({
-  admin: {
-    type: Boolean,
-    default: false,
-  },
-  nombre: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  reservaciones: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Reservaciones',
+const usuariosSchema = new Schema(
+  {
+    admin: {
+      type: Boolean,
+      default: false,
     },
-  ],
-})
+    nombre: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    reservaciones: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Reservaciones',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
 
 const Usuarios = mongoose.model('Usuarios', usuariosSchema)
 
